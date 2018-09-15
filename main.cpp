fb5579ec7f48809aec60ccaa8f76361c675cbec9
Rodrigo Bedoya <rodrigo.bedoya@utec.edu.pe>
2018-09-14 19:28:45 -0500

    implemented vector

--- main.cpp
+++ main.cpp
@@ -5,10 +5,14 @@
 
 using namespace std;
 
+
+int conv(int *start, int *current,int *size_current, int dimensions);
+
+
 template <typename T>
 struct Dimensions {      
     int operator()(int* coordinates, int* sizes, int dimensions) {
-        // TODO
+        return conv(coordinates, coordinates, sizes, dimensions);
     }
 };
 
@@ -18,6 +22,31 @@ struct Integer {
 };
 
 int main(int argc, char *argv[]) {
-    system("pause");
-    return EXIT_SUCCESS;
+    //system("pause");
+    //=======TEST CASE==========
+    int coordinates[] = {1,1,1,1};
+    int sizes[] = {2,2,2,2};
+    int *current_size = sizes;
+    int *current = coordinates;
+	Vector<Integer> myVector(4,current_size);
+	myVector.set(2,current);
+	cout << myVector.get(current) << endl;
+    
+  return EXIT_SUCCESS;
+}
+
+int conv(int *start, int *current,int *size_current, int dimensions)
+{
+	
+	int p1 = *current, p2 = *size_current, p3 = 0;
+	if((start+dimensions-1)!=(current+1))
+	{
+		p3 = conv(start,current+1,size_current+1,dimensions);
+	}
+	else
+	{
+		p3 = *(current+1);
+	}
+
+	return p1+p2*p3; 
 }
--- vector.h
+++ vector.h
@@ -1,6 +1,8 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <stdexcept>
+
 template <typename Tr>
 class Vector {
     public:
@@ -13,17 +15,51 @@ class Vector {
 
         int dimensions;
         int* dimensionSizes;
+        Operation conv;
 
     public:
-        Vector() : data(nullptr) {};
+        Vector() : data(NULL) {};
              
-        Vector(int dimensions, int* dimensionSizes) : dimensions(dimensions), dimensionSizes(dimensionSizes) {
-            // TODO
+        Vector(int dimensions, int* dimensionSizes) : 
+            dimensions(dimensions), 
+            dimensionSizes(dimensionSizes),
+            dataSize(1) 
+        {
+            for(int i = 0; i < dimensions; i++)
+            {
+                dataSize *= *dimensionSizes;
+                dimensionSizes++;
+            }
+            data = new T[dataSize];
         }
              
-        void set(T datum, int* coordinates); // TODO
+        void set(T datum, int* coordinates)
+        {
+            int position =conv(coordinates,dimensionSizes,dimensions);
+            
+            if (position >= dataSize)
+            {
+                throw std::out_of_range("out of range");
+            }
+
+            T* temporal = data+position;
+            *temporal = datum;
+            return;
+        }
              
-        T get(int* coordinates); // TODO
+        T get(int* coordinates)
+        {
+            int position = conv(coordinates,dimensionSizes,dimensions);
+            
+
+            if (position >= dataSize)
+            {
+                throw std::out_of_range("out of range");
+            }
+
+            T* temporal = data+position;
+            return *temporal;
+        }
 };
 
 #endif
